2d020ccbbe9a4379d7f2a7ee2c42d8c80fec2b3d
Sandro Motyl <sandro.motyl@softplan.com.br>
2021-03-31 01:33:21 -0300

    fix: remove finglet and install child_proccess

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 'use strict';
 
 const { spawnSync } = require('child_process');
-const figlet = require('figlet');
 
 const scriptNames = [ 'start' ];
 
@@ -14,23 +13,14 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-figlet('basic-react-scripts', (err, data) => {
-  if (err) {
-    console.log('Error');
-    process.exit(-1);
-  } else {
-    console.log(`\n${data}\n`);
-  }
-
-  if (scriptNames.includes(script)) {
-    const result = spawnSync(
-      'node',
-      [`${__dirname}/scripts/${script}.js`, ...process.argv.slice(3)],
-      { stdio: 'inherit' }
-    );
-    process.exit(result.status);
-  } else {
-    console.log(`Unknown script ${script}.`);
-    process.exit(1);
-  }
-});
+if (scriptNames.includes(script)) {
+  const result = spawnSync(
+    'node',
+    [`${__dirname}/scripts/${script}.js`, ...process.argv.slice(3)],
+    { stdio: 'inherit' }
+  );
+  process.exit(result.status);
+} else {
+  console.log(`Unknown script ${script}.`);
+  process.exit(1);
+}
