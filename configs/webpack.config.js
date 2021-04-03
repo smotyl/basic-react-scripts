9d0cfddd80ecbf55e9baa4191fc46e4bfeb0d47e
Sandro Motyl <sandro.motyl@softplan.com.br>
2021-04-03 15:13:04 -0300

    feat: create ts config

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -4,14 +4,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const currDir = process.cwd();
 
 module.exports = (config) => {
-  const { mode } = config;
+  const { mode, typescript } = config;
+
+  let babelLoader = {
+    test: /\.(ts|js)x?$/,
+    loader: require.resolve('babel-loader'),
+    exclude: /node_modules/,
+    options: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    }
+  };
+
+  let tsLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  let entryFile = typescript ? 'index.tsx' : 'index.js'
 
   return {
     mode: mode,
-    entry: path.resolve(currDir, 'src', 'index.js'),
+    entry: path.resolve(currDir, 'src', entryFile),
     output: {
       path: path.resolve(currDir, 'dist'),
       filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js', '.jsx' ],
     },
     devServer: {
       contentBase: path.resolve(currDir, 'src'),
@@ -24,14 +45,7 @@ module.exports = (config) => {
     ],
     module: {
       rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          loader: require.resolve('babel-loader'),
-          exclude: /node_modules/,
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          }
-        },
+        typescript ? tsLoader : babelLoader,
       ]
     }
   }
