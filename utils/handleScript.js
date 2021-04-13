8d63d67e65095e1d78d1aadbf674b503d8ee23e6
Sandro Motyl <sandro.motyl@softplan.com.br>
2021-04-13 15:00:17 -0300

    fix: Use __dirname folder to get scripts

--- utils/handleScript.js
+++ utils/handleScript.js
@@ -1,11 +1,9 @@
 const { spawnSync } = require('child_process');
 const { AVAIBLE_SCRIPTS } = require('./constants');
 
-const currDir = process.cwd();
-
 const executeScript = (script) => spawnSync(
   'node',
-  [`${currDir}/scripts/${script}.js`, ...process.argv.slice(3)],
+  [`${__dirname}/scripts/${script}.js`, ...process.argv.slice(3)],
   { stdio: 'inherit' }
 );
 
